12cbb6df73c0a5af078750f777f0f9916425d60c
kelvson <kelvsonnilson01@gmail.com>
2023-01-25 18:02:03 -0300

    version 1.0

--- main.c
+++ main.c
@@ -2,24 +2,64 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MAX_OPTIONS_CQ 3
+#define MAX_OPTIONS_CQ 4
+#define MAX_OPTIONS_CQ_30CM 4
+#define MAX_OPTIONS_CQ_PREMIUNS 4
+#define MAX_OPTIONS_CQ_30CM_PREMIUNS 4
 #define MAX_OPTIONS_PR 2
 
 char produto[30];
 double valor;
 
+struct Produto{
+    char nome[40];
+    double valor;
+};
+
+struct Produto cachorro_quente_opcoes[MAX_OPTIONS_CQ + MAX_OPTIONS_CQ_30CM] = {
+    {"C.Q. Tradicional", 25.00},
+    {"C.Q. Carne ", 28.00},
+    {"C.Q. Frango", 28.00},
+    {"C.Q. Charque", 29.00},
+    {"C.Q. Tradicional 30CM", 15.00},
+    {"C.Q. Carne 30CM", 16.50},
+    {"C.Q. Frango 30CM", 16.50},
+    {"C.Q. Charque 30CM", 17.00}
+};
+
+
+struct Produto cachorro_quente_premium_opcoes[MAX_OPTIONS_CQ_PREMIUNS + MAX_OPTIONS_CQ_30CM_PREMIUNS] = {
+    {"C.Q. Tradicional PREMIUM", 30.00},
+    {"C.Q. Carne PREMIUM ", 33.00},
+    {"C.Q. Frango PREMIUM", 33.00},
+    {"C.Q. Charque PREMIUM", 34.00},
+    {"C.Q. Tradicional 30CM PREMIUM", 18.00},
+    {"C.Q. Carne 30CM PREMIUM", 19.50},
+    {"C.Q. Frango 30CM PREMIUM", 19.50},
+    {"C.Q. Charque 30CM PREMIUM", 20.00}
+};
+
+struct Produto prensado_opcoes[MAX_OPTIONS_PR] = {
+    {"Prensado de Frango", 14.00},
+    {"Prensado de Bacon", 15.00}
+};
+
+struct Pessoa{
+    char nome[20];
+    int bloco;
+    int apt;
+};
+
 int menu();
 int quantidade();
 
 void cachorro_quente();
+void cachorro_quente_premium(int quantidade);
 void prensado();
-void cachorro_quente_nomes(int index);
-void prensado_nomes(int index);
-
 
 int main(){
 
-    int c = 1;
+    int c;
     double soma = 0;
     char end_cliente[20];
 
@@ -36,10 +76,9 @@ int main(){
                 prensado();
                 break;
         }
-
         if(c == 0) break;
-
         soma += valor;
+
     }
 
     printf("%.2f\n", soma);
@@ -61,7 +100,9 @@ int menu(){
         printf("Digite a opcao: ");
 
         fflush(stdin);
-        if(scanf("%d", &t) != 1) t -= 1;
+        if(scanf("%d", &t) != 1){
+            printf("Opcao invalida! Tente novamente.");
+        }
 
         system("cls");
     } while(t < 0 || t > 3);
@@ -72,51 +113,75 @@ int menu(){
 
 
 // Quantidade de produtos:
-
 int quantidade(){
-    int qntd, c = 0;
-    do{
-        printf("Entre aqui a quantidade desejada: ");
-        if(scanf("%d", &qntd) != 1) qntd -= 1;
-    } while(qntd != 1);
-    return qntd;
+    int total;
+    printf("Digite a quantidade que deseja: ");
+    while(scanf("%d", &total) != 1){
+        printf("\nEntrada invalida. Por favor, entre com um numero inteiro valido: ");
+        fflush(stdin);
+    }
+    return total;
 }
+
 
 // Momento do Cachorro quente:
 void cachorro_quente(){
-    int qntd = quantidade();
-    double valores[MAX_OPTIONS_CQ] = { 25.00, 28.00, 31.00 };
     int selectedoption;
+    int qntd = quantidade();
 
     fflush(stdin);
-        
+
     do{
         printf("%5sCachorros Quentes\n", " ");
         printf("%10sSabores:\n\n", " ");
-        printf("[1] Tradicional\n");
-        printf("[2] Carne\n");
-        printf("[3] Premium\n");
-        printf("Escolha o sabor [0 para voltar]: ");
+        
+        for(int i=1; i<=MAX_OPTIONS_CQ; i++) printf("[%d] %s \t[%d] %s\n", i, cachorro_quente_opcoes[i-1].nome, i+4, cachorro_quente_opcoes[i+3].nome);
 
-        if(scanf("%d", &selectedoption) != 1) selectedoption -= 1;
+        printf("Escolha o sabor [0 para voltar][%d para os premiuns]: ", MAX_OPTIONS_CQ+MAX_OPTIONS_CQ_30CM+1); // Para caso adicione mais produtos
+
+        if(scanf("%d", &selectedoption) != 1){
+            printf("Entrada invalida. Tente novamente: ");
+            selectedoption -= 1;
+        }
     
         if(selectedoption == 0) menu();
-    } while(selectedoption < 0 || selectedoption > MAX_OPTIONS_CQ);
+    } while(selectedoption < 0 || selectedoption > MAX_OPTIONS_CQ + MAX_OPTIONS_CQ_30CM + 1);
 
-    cachorro_quente_nomes(selectedoption);
-    valor = valores[selectedoption - 1] * qntd;
+    if(selectedoption == MAX_OPTIONS_CQ+MAX_OPTIONS_CQ_30CM+1) cachorro_quente_premium(qntd);
+
+    else {
+        valor = cachorro_quente_opcoes[selectedoption - 1].valor * qntd;
+        strcpy(produto, cachorro_quente_opcoes[selectedoption - 1].nome);
+    }
+
 }
 
-// Nome do cachorro quente:
-void cachorro_quente_nomes(int index){
-    char nomes[MAX_OPTIONS_CQ][20] = {"C.Q Tradicional", "C.Q Carne", "Q.C Premium"};
-    strcpy(produto, nomes[index - 1]);
-}
+// Cachorros quente premiuns:
+void cachorro_quente_premium(int quantidade){
+    int selectedoptionp;
+    
+    fflush(stdin);
 
+    do{
+        system("cls");
+        for(int i=1; i<=MAX_OPTIONS_CQ; i++) printf("[%d] %s \t[%d] %s\n", i, cachorro_quente_premium_opcoes[i-1].nome, i+4, cachorro_quente_premium_opcoes[i+3]);
+        printf("Escolha o sabor do cachorro quente premium [0 para voltar]: ");
+
+        if(scanf("%d", &selectedoptionp) != 1){
+            printf("Entrada invalida. Tente novamente.");
+            selectedoptionp -= 1;
+        }
+
+        if(selectedoptionp == 0) menu();
+    } while(selectedoptionp < 0 || selectedoptionp > MAX_OPTIONS_CQ_PREMIUNS+MAX_OPTIONS_CQ_30CM_PREMIUNS);
+
+    valor = cachorro_quente_premium_opcoes[selectedoptionp - 1].valor * quantidade;
+    strcpy(produto, cachorro_quente_premium_opcoes[selectedoptionp - 1].nome);
+    
+}
 
 // Momento do Prensado:
 void prensado(){
-    double valores[MAX_OPTIONS_PR] = { 14.00, 15.00 };
     int selectedoption;
     int qntd = quantidade();
 
@@ -125,21 +190,19 @@ void prensado(){
     do{
         printf("%5sCachorros Quentes\n", " ");
         printf("%10sSabores:\n\n", " ");
-        printf("[1] Frango\n");
-        printf("[2] Bacon\n");
+
+        for(int i = 1; i<MAX_OPTIONS_PR; i++) printf("[%d] %s\n", i, prensado_opcoes[i-1].nome);
+
         printf("Escolha o sabor [0 para voltar]: ");
 
-        if(scanf("%d", &selectedoption) != 1) selectedoption -= 1;
+        if(scanf("%d", &selectedoption) != 1){
+            printf("Entrada invalida. Tente novamente: ");
+            selectedoption -= 1;
+        }
     
         if(selectedoption == 0) menu();
     } while(selectedoption < 0 || selectedoption > MAX_OPTIONS_PR);
 
-    prensado_nomes(selectedoption);
-    valor = valores[selectedoption - 1] * qntd;
+    valor = prensado_opcoes[selectedoption - 1].valor * qntd;
+    strcpy(produto, prensado_opcoes[selectedoption - 1].nome);
 }
-
-void prensado_nomes(int index){
-    char nomes[MAX_OPTIONS_PR][20] = {"Prens. Frango", "Prens. Bacon"};
-    strcpy(produto, nomes[index - 1]);
-}
-
